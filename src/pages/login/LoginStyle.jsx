a11a4007fca42c0a5f3f17c672fc821eb1429bca
leeheezin <gmlwls107@naver.com>
2023-10-17 18:15:47 +0900

    Feat: login페이지 생성 마크업

---
+++ src/pages/login/LoginStyle.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+    display: flex;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height: 100vh;
+    background: #CB9367;
+`
+export const Logo = styled.div`
+    margin: auto;
+    h1 {
+        width: 144px;
+        img {
+            width: 100%;
+        }
+    }
+`
