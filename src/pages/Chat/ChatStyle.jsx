5cf666c95cda85570b9a308ff78e29c64151d4a1
예람 <dpfka97@daum.net>
2023-10-19 18:15:59 +0900

    Design:Chat List 수정중

---
+++ src/pages/Chat/ChatStyle.jsx
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const Body = styled.div`
+  /* border: 3px dashed gray; */
+    height: 100vh;
+    width: 390px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+  /* transform: translateX(-10%); */
+`;
+
+// top bar
+export const TopBar = styled.div`
+    height: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 0.5px solid var(--DBDBDB, #dbdbdb);
+    button {
+    margin: 13px 16px;
+    background: #fff;
+    border-style: none;
+    }
+`;
+// top bar
+
+// section 1
+export const Sect1 = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Chat1 = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+    gap: 10px;
+	img {
+        padding: 10px 10px 10px 10px ;
+        width: 42px;
+        height: 42px;
+    } 
+    div {
+        font-size: 14px;
+        width: 500px;
+        height: 30px;
+        line-height: 7px;
+        justify-content: space-between;
+    }
+    // date {
+    //     margin-left: auto;
+    //     font-size: 10px;
+    // }
+
+    .text {
+        display: flex;
+        justify-content : space-between;
+    }
+`;
+
+// export const Chat2 = styled.div`
+//     display: flex;
+//     justify-content: space-between;
+//     margin-top: 10px;
+//     gap: 10px;
+// 	img {
+//         width: 42px;
+//         height: 42px;
+// } 
+//     div {
+//         font-size: 14px;
+//         width: 500px;
+//         height: 30px;
+//         line-height: 7px;
+//         justify-content: space-between;
+//     }
+// `;
