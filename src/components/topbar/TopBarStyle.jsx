7ebd217f945a35f9ea15fb89da374f2e3b27834b
예람 <dpfka97@daum.net>
2023-10-27 14:13:11 +0900

    Design: 수정

--- src/components/topbar/TopBarStyle.jsx
+++ src/components/topbar/TopBarStyle.jsx
@@ -10,28 +10,31 @@ export const Top = styled.div`
   justify-content: space-between;
   border-bottom: 0.5px solid var(--DBDBDB, #dbdbdb);
   padding: 0 20px;
+
   button {
     margin: 13px 16px;
     background: #fff;
     border-style: none;
   }
+  
   .btn-save {
     color: #fff;
-    background: #12184E;
+    background: #12184e;
     padding: 7px 32px;
     border-radius: 32px;
-    cursor: not-allowed;
+    /* cursor: not-allowed; */
+
+    &.disabled {
+      color: #fff;  
+      background: #6f76b6;
+    }
   }
+
   .active {
     color: #fff;
     background: #12184e;
     padding: 7px 32px;
     border-radius: 32px;
-
-    &.disabled {
-      color: #fff; /* 비활성화 시 텍스트 색상 */
-      background: #6f76b6; /* 비활성화 시 배경색 */
-    }
   }
 `;
 
